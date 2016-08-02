5689dd4faf8c54488002a5ad187e803b2719926e
Cícero Augusto <cicerolp@gmail.com>
2016-08-02 13:39:04 -0300

    update query algorithm without inline serialization

--- source/BinnedPivot.h
+++ source/BinnedPivot.h
@@ -21,9 +21,15 @@ public:
       return pivot < other.pivot;
    };
 
+   static inline bool upper_bound_comp(const BinnedPivot& lhs, const Pivot& rhs);
+
    Pivot pivot;
    uint64_t value;
 };
+
+inline bool BinnedPivot::upper_bound_comp(const BinnedPivot& lhs, const Pivot& rhs) {
+   return Pivot::upper_bound_comp(lhs.pivot, rhs);
+}
 
 using range_container = std::vector<BinnedPivot>;
 using range_iterator = range_container::const_iterator;
--- source/Dimension.cpp
+++ source/Dimension.cpp
@@ -8,52 +8,58 @@ void Dimension::restrict(range_container& range, response_container& response, b
 
    if (range.size() == 0) return;
 
-   pivot_iterator iters_it;
    pivot_iterator it_lower;
+   pivot_iterator it_upper;
 
    for (const auto& el : subset) {
 
-      iters_it = el->pivots.begin();
+      it_lower = el->pivots.begin();
 
-      for (const auto& r : range) {
+      for (auto it_range = range.begin(); it_range != range.end(); ++it_range) {
 
-         if (iters_it == el->pivots.end()) {
-            break;
+         if (it_lower == el->pivots.end()) break;
 
-         } else if (r.pivot.begins_after(*iters_it)) {
-            it_lower = std::lower_bound(iters_it, el->pivots.end(), r.pivot, Pivot::lower_bound_comp);
-            if (it_lower == el->pivots.end()) break;
+      search:
+         if ((*it_range).pivot.begins_after(*it_lower)) {
+            // binnary search to find subset iterator
+            it_upper = std::lower_bound(it_lower, el->pivots.end(), (*it_range).pivot, Pivot::lower_bound_comp);
+            if (it_upper == el->pivots.end()) break;
 
-         } else if (r.pivot.ends_before(*iters_it)) {
-            continue;
+         } else if ((*it_range).pivot.ends_before(*it_lower)) {
+            // binary search to find range iterator
+            it_range = std::lower_bound(it_range, range.end(), (*it_lower), BinnedPivot::upper_bound_comp);
 
+            if (it_range != range.end()) {
+               goto search;
+            } else {
+               break;
+            }
          } else {
-            it_lower = iters_it;
+            it_upper = it_lower;
          }
 
          switch (option) {
             case CopyValueFromRange:
-               while (it_lower != el->pivots.end() && r.pivot >= (*it_lower)) {
-                  response.emplace_back((*it_lower), r.value);
-                  it_lower++;
+               while (it_upper != el->pivots.end() && (*it_range).pivot >= (*it_upper)) {
+                  response.emplace_back((*it_upper), (*it_range).value);
+                  ++it_upper;
                }
                break;
             case CopyValueFromSubset:
-               while (it_lower != el->pivots.end() && r.pivot >= (*it_lower)) {
-                  response.emplace_back((*it_lower), el->value);
-                  it_lower++;
+               while (it_upper != el->pivots.end() && (*it_range).pivot >= (*it_upper)) {
+                  response.emplace_back((*it_upper), el->value);
+                  ++it_upper;
                }
-               
                break;
             default:
-               while (it_lower != el->pivots.end() && r.pivot >= (*it_lower)) {
-                  it_lower++;
+               while (it_upper != el->pivots.end() && (*it_range).pivot >= (*it_upper)) {
+                  ++it_upper;
                }
-               response.insert(response.end(), iters_it, it_lower);
+               response.insert(response.end(), it_lower, it_upper);
                break;
          }
 
-         iters_it = it_lower;
+         it_lower = it_upper;
       }
    }
 
