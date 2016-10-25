07255b4b641f41bf8b0c04ae058912c024850bf1
Cícero Pahins <cicerolp@gmail.com>
2016-10-25 03:16:31 -0300

    change to CMake building

--- WebContent/js/view_schemas.js
+++ WebContent/js/view_schemas.js
@@ -134,30 +134,6 @@ var view_schemas = {
                     "15", "16", "17", "18", "19", "20", "21", "22",
                     "23"]
              }
-          },
-          {
-             type: "binned-scatterplot",
-             title: "Day of Week x Hour of Day",
-             on_menu: false,
-             div: "#top-section",
-             size: 25,
-             field: {
-                name: "binned"
-             },
-             field_x: {
-                name: "c_2",
-                title: "Hour of Day",
-                values: ["00", "01", "02", "03", "04", "05", "06",
-                    "07", "08", "09", "10", "11", "12", "13", "14",
-                    "15", "16", "17", "18", "19", "20", "21", "22",
-                    "23"]
-             },
-             field_y: {
-                name: "c_1",
-                title: "Day of Week",
-                values: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
-                    "Sun"]
-             }
           }]
    },
 
---
+++ source/stdafx.h
@@ -0,0 +1,56 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+
+#pragma once
+
+#define RAPIDJSON_SSE42
+
+// Windows specific defines
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#define _USE_MATH_DEFINES
+#define _CRT_SECURE_NO_WARNINGS
+
+// C includes
+#include <cmath>
+#include <cstdio>
+#include <cassert>
+#include <cstdint>
+
+// C++ includes
+#include <map>
+#include <regex>
+#include <array>
+#include <queue>
+#include <tuple>
+#include <thread>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <unordered_map>
+
+// boost library
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
+// ResidentSetSize
+#include <nadeau/ResidentSetSize.h>
+
+// mongoose
+#include <mongoose.h>
+
+// rapidjson
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+
+// dynarray
+#include <robbepop/dynarray.hpp>
+
+// timsort
+#include <gfx/timsort.hpp>
+
+#include "types.h"
