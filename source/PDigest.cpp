cicerolp/qds

commit b38ae478c658b90bbe2f51dc15c8014094555738

fix p-digest buffer allocation

source/PDigest.cpp
@@ -45,11 +45,7 @@ std::vector<float> PDigest::get_payload(Data &data, const Pivot &pivot) {
 
     float projectedW = wSoFar + proposedWeight;
 
-    bool addThis = false;
-
-    addThis = projectedW <= wLimit;
-
-    if (addThis) {
+    if (projectedW <= wLimit) {
       weight_equals_one = false;
 
       // next point will fit
@@ -86,6 +82,8 @@ std::vector<float> PDigest::get_payload(Data &data, const Pivot &pivot) {
     // payload does not contains weight array
     payload.emplace_back(0.f);
 
+    assert(payload.size() == (lastUsedCell + 1));
+
   } else {
     payload.reserve(lastUsedCell * 2 + 1);
 
@@ -95,6 +93,8 @@ std::vector<float> PDigest::get_payload(Data &data, const Pivot &pivot) {
 
     // payload contains weight array
     payload.emplace_back(1.f);
+
+    assert(payload.size() == (lastUsedCell * 2 + 1));
   }
 #else
   payload.reserve(lastUsedCell * 2);
@@ -192,7 +192,7 @@ uint32_t AgrrPDigest::merge(size_t payload_index, const Pivot &pivot) {
 
     size_t curr = _buffer_mean.size();
     _buffer_mean.resize(curr + payload_size);
-    _buffer_weight.resize(curr + payload_size);
+    _buffer_weight.reserve(curr + payload_size);
 
     // insert mean data
     std::memcpy(&(_buffer_mean[curr]), &payload.lower[0], payload_size * sizeof(float));
@@ -250,10 +250,10 @@ uint32_t AgrrPDigest::merge(size_t payload_index, const pivot_it &it_lower, cons
 
       size_t curr = _buffer_mean.size();
       _buffer_mean.resize(curr + payload_size);
-      _buffer_weight.resize(curr + payload_size);
+      _buffer_weight.reserve(curr + payload_size);
 
       // insert mean data
-      std::memcpy(&(_buffer_mean[curr]), &payload.lower[0], payload.upper - payload.lower);
+      std::memcpy(&(_buffer_mean[curr]), &payload.lower[0], payload_size * sizeof(float));
       // insert weight data
       _buffer_weight.insert(_buffer_weight.end(), payload_size, 1.f);
 
@@ -273,7 +273,7 @@ uint32_t AgrrPDigest::merge(size_t payload_index, const pivot_it &it_lower, cons
     ++it;
   }
 #else
-  /*auto it = it_lower;
+  auto it = it_lower;
   // insert payload data
   while (it != it_upper) {
     count += (*it).size();
@@ -291,7 +291,7 @@ uint32_t AgrrPDigest::merge(size_t payload_index, const pivot_it &it_lower, cons
       std::memcpy(&(_buffer_weight[curr]), &payload.lower[payload_size], payload_size * sizeof(float));
 
     ++it;
-  }*/
+  }
 #endif // PDIGEST_OPTIMIZE_ARRAY
 
   if (_buffer_mean.size() >= PDIGEST_BUFFER_SIZE) {
@@ -306,19 +306,14 @@ float AgrrPDigest::quantile(float q) {
     merge_buffer_data();
   }
 
-  if (_lastUsedCell == 0 && _weight[_lastUsedCell] == 0) {
+  if (_lastUsedCell == 0) {
     // no centroids means no data, no way to get a quantile
     return std::numeric_limits<float>::quiet_NaN();
-
-  } else if (_lastUsedCell == 0) {
-    // with one data point, all quantiles lead to Rome
-    return _mean[0];
   }
 
   // we know that there are at least two centroids now
   int32_t n = _lastUsedCell;
 
-  //float totalWeight = std::accumulate(_weight.begin(), _weight.begin() + _lastUsedCell, 0.f);
   float totalWeight = 0.f;
   for (auto i = 0; i < _lastUsedCell; ++i) {
     totalWeight += _weight[i];
@@ -361,7 +356,7 @@ float AgrrPDigest::inverse(float value) {
     merge_buffer_data();
   }
 
-  if (_lastUsedCell == 0 && _weight[_lastUsedCell] == 0) {
+  if (_lastUsedCell == 0) {
     // no centroids means no data, no way to get a quantile
     return std::numeric_limits<float>::quiet_NaN();
   }

source/Aggr.h
@@ -14,6 +14,7 @@ class Aggr {
  public:
   Aggr() = default;
   Aggr(const Query::aggr_expr &expr, size_t __index) : _expr(expr), _payload_index(__index) {};
+  virtual ~Aggr() = default;
 
  protected:
   virtual inline void write_value(uint64_t value, json &writer) const {
@@ -40,6 +41,7 @@ class Aggr {
 class AggrGroupBy : public Aggr {
  public:
   AggrGroupBy(const Query::aggr_expr &expr, size_t __index) : Aggr(expr, __index) {};
+  virtual ~AggrGroupBy() = default;
 
   virtual void merge(uint64_t value, const pivot_it &it_lower, const pivot_it &it_upper) = 0;
 
@@ -79,6 +81,7 @@ class AggrSummarize : public Aggr {
  public:
   AggrSummarize() = default;
   AggrSummarize(const Query::aggr_expr &expr, size_t __index) : Aggr(expr, __index) {};
+  virtual ~AggrSummarize() = default;
 
   virtual void merge(const pivot_it &it_lower, const pivot_it &it_upper) = 0;
   virtual void merge(const range_it &it_lower, const range_it &it_upper) = 0;
@@ -106,6 +109,7 @@ class AggrCountGroupBy : public AggrGroupBy {
  public:
   AggrCountGroupBy(const Query::aggr_expr &expr, size_t __index) :
       AggrGroupBy(expr, __index) {}
+  virtual ~AggrCountGroupBy() = default;
 
   virtual void merge(uint64_t value, const pivot_it &it_lower, const pivot_it &it_upper) override {
     auto it = it_lower;
@@ -152,6 +156,7 @@ class AggrCountSummarize : public AggrSummarize {
   AggrCountSummarize() = default;
   AggrCountSummarize(const Query::aggr_expr &expr, size_t __index) :
       AggrSummarize(expr, __index) {}
+  virtual ~AggrCountSummarize() = default;
 
   void merge(const pivot_it &it_lower, const pivot_it &it_upper) override {
     auto it = it_lower;
@@ -186,6 +191,7 @@ class AggrPayloadGroupBy : public AggrGroupBy {
  public:
   AggrPayloadGroupBy(const Query::aggr_expr &expr, size_t __index) :
       AggrGroupBy(expr, __index) {}
+  virtual ~AggrPayloadGroupBy() = default;
 
   void merge(uint64_t value, const pivot_it &it_lower, const pivot_it &it_upper) override {
     uint32_t count = _map[value].payload.merge(_payload_index, it_lower, it_upper);
@@ -218,6 +224,7 @@ class AggrPayloadSummarize : public AggrSummarize {
   AggrPayloadSummarize() = default;
   AggrPayloadSummarize(const Query::aggr_expr &expr, size_t __index) :
       AggrSummarize(expr, __index) {}
+  virtual ~AggrPayloadSummarize() = default;
 
   void merge(const pivot_it &it_lower, const pivot_it &it_upper) override {
     _map.merge(_payload_index, it_lower, it_upper);
@@ -240,6 +247,7 @@ class AggrPDigestGroupBy : public AggrPayloadGroupBy<AgrrPDigest> {
  public:
   AggrPDigestGroupBy(const Query::aggr_expr &expr, size_t __index) :
       AggrPayloadGroupBy(expr, __index) {}
+  virtual ~AggrPDigestGroupBy() = default;
 
   void output(json &writer) override {
     auto parameters = AgrrPDigest::get_parameters(_expr);
@@ -436,6 +444,7 @@ class AggrPDigestSummarize : public AggrPayloadSummarize<AgrrPDigest> {
   AggrPDigestSummarize() = default;
   AggrPDigestSummarize(const Query::aggr_expr &expr, size_t __index) :
       AggrPayloadSummarize(expr, __index) {}
+  virtual ~AggrPDigestSummarize() = default;
 
   void output(json &writer) override {
     output(AgrrPDigest::get_parameters(_expr), writer);
@@ -484,6 +493,7 @@ class AggrGaussianGroupBy : public AggrPayloadGroupBy<AggrGaussian> {
  public:
   AggrGaussianGroupBy(const Query::aggr_expr &expr, size_t __index) :
       AggrPayloadGroupBy(expr, __index) {}
+  virtual ~AggrGaussianGroupBy() = default;
 
   void output(json &writer) override {
     if (_expr.first == "variance") {
@@ -525,6 +535,7 @@ class AggrGaussianSummarize : public AggrPayloadSummarize<AggrGaussian> {
   AggrGaussianSummarize() = default;
   AggrGaussianSummarize(const Query::aggr_expr &expr, size_t __index) :
       AggrPayloadSummarize(expr, __index) {}
+  virtual ~AggrGaussianSummarize() = default;
 
   void output(json &writer) override {
     if (_expr.first == "variance") {

source/Gaussian.h
@@ -18,6 +18,8 @@ class Gaussian : public Payload {
 
 class AggrGaussian : public AgrrPayload {
  public:
+  virtual ~AggrGaussian() = default;
+
   inline bool empty() const override {
     return count_i.size() == 0;
   }

source/PDigest.h
@@ -101,6 +101,8 @@ class PDigest : public Payload {
 
 class AgrrPDigest : public AgrrPayload {
  public:
+  virtual ~AgrrPDigest() = default;
+
   inline bool empty() const override {
     return _lastUsedCell == 0;
   }

commit 3140d2ace886bc35cc39122bad5aa579241bf3e2

fix regression introduced by b38ae47

source/PDigest.cpp
@@ -192,7 +192,7 @@ uint32_t AgrrPDigest::merge(size_t payload_index, const Pivot &pivot) {
 
     size_t curr = _buffer_mean.size();
     _buffer_mean.resize(curr + payload_size);
-    _buffer_weight.reserve(curr + payload_size);
+    // _buffer_weight.reserve(curr + payload_size);
 
     // insert mean data
     std::memcpy(&(_buffer_mean[curr]), &payload.lower[0], payload_size * sizeof(float));
@@ -250,7 +250,7 @@ uint32_t AgrrPDigest::merge(size_t payload_index, const pivot_it &it_lower, cons
 
       size_t curr = _buffer_mean.size();
       _buffer_mean.resize(curr + payload_size);
-      _buffer_weight.reserve(curr + payload_size);
+      // _buffer_weight.reserve(curr + payload_size);
 
       // insert mean data
       std::memcpy(&(_buffer_mean[curr]), &payload.lower[0], payload_size * sizeof(float));