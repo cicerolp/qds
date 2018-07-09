fd157039d8cbe90f4933f76bb6fad1a4f6b19e22
Cícero Pahins <cicerolp@gmail.com>
2018-07-09 11:15:27 -0300

    update benchmark script

--- source/util/benchmark_util.cpp
+++ source/util/benchmark_util.cpp
@@ -2,9 +2,9 @@
 // Created by cicerolp on 7/9/18.
 //
 
+#include "stdafx.h"
+#include "benchmark_util.h"
+
 // ENABLE_TIMMING
 uint32_t TIMER_ID = 0;
 uint32_t TIMER_IT = 0;
-
-#include "stdafx.h"
-#include "benchmark_util.h"
