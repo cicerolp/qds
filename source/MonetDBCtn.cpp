100b203881554547f798f05a924a5f053aad8257
Cícero Pahins <cicerolp@gmail.com>
2018-12-06 07:49:51 -0200

    MonetDB create and insert using flights schema

--- source/MonetDBCtn.cpp
+++ source/MonetDBCtn.cpp
@@ -107,11 +107,112 @@ void MonetDBCtn::insert_snap(const std::string &filename) {
 }
 
 void MonetDBCtn::create_on_time() {
-
+#ifdef __GNUC__
+  update(_dbh, (char *) "DROP TABLE IF EXISTS db");
+  update(_dbh,
+         (char *) "CREATE TABLE db (on_time INTEGER, unique_carrier INTEGER, crs_dep_time TIMESTAMP, origin_airport POINT, dep_delay_t REAL)"
+  );
+#endif // __GNUC__
 }
 
 void MonetDBCtn::insert_on_time(const std::string &filename) {
+#ifdef __GNUC__
 
+  if (!_init) {
+    return;
+  }
+
+  static const std::string sep = ",";
+
+  // source: https://bravenewmethod.com/2016/09/17/quick-and-robust-c-csv-reader-with-boost/
+  // used to split the file in lines
+  const boost::regex linesregx("\\r\\n|\\n\\r|\\n|\\r");
+
+  // used to split each line to tokens, assuming ',' as column separator
+  const boost::regex fieldsregx(sep + "(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+  std::ifstream infile(filename);
+
+  std::string line;
+
+  // skip header
+  std::getline(infile, line);
+
+  MapiHdl hdl = mapi_prepare(_dbh, "INSERT INTO db VALUES (?, ?, ?, ?, ?)");
+  check(_dbh, hdl);
+
+  while (!infile.eof()) {
+
+    std::getline(infile, line);
+
+    if (line.empty()) {
+      continue;
+    }
+
+    try {
+      // split line to tokens
+      boost::sregex_token_iterator ti(line.begin(), line.end(), fieldsregx, -1);
+      boost::sregex_token_iterator ti_end;
+
+      std::vector<std::string> data(ti, ti_end);
+
+      // (char *) "CREATE TABLE db (on_time INTEGER, unique_carrier INTEGER, crs_dep_time TIMESTAMP, origin_airport POINT, dep_delay_t REAL)"
+      /*
+      00, arr_on_time
+      01, dep_on_time
+      02, cancelled
+      03, diverted
+      04, unique_carrier
+      05, origin_airport_id
+      06, dest_airport_id
+      07, crs_dep_time
+      08, crs_arr_time
+      09, origin_airport
+      10, origin_airport
+      11, dest_airport
+      12, dest_airport
+      13, dep_delay
+      14, dep_delay_minutes
+      15, arr_delay
+      16, arr_delay_minutes
+      17, actual_elapsed_time
+      18, air_time
+      19, distance
+      */
+
+      const long epoch = std::stoul(data[7]);
+      std::stringstream ss;
+      ss << std::put_time(std::localtime(&epoch), "%FT%TZ");
+      auto crs_dep_time = ss.str();
+
+      std::string date = "(SELECT sys.str_to_timestamp('" + crs_dep_time + "', '%Y-%m-%dT%H:%M:%SZ'))";
+
+      mapi_param(hdl, 1, (char **) date.c_str());
+
+      mapi_param(hdl, 0, (char **) data[1].c_str());
+      mapi_param(hdl, 1, (char **) data[4].c_str());
+      mapi_param(hdl, 2, (char **) date.c_str());
+
+      std::string coord = "'POINT( " + data[3] + " " + data[2] + " )'";
+      mapi_param(hdl, 3, (char **) coord.c_str());
+
+      mapi_param(hdl, 4, (char **) data[13].c_str());
+
+      int ret = mapi_execute(hdl);
+      check(_dbh, hdl, ret);
+
+    } catch (const std::exception &e) {
+      std::cerr << "[" << e.what() << "]: [" << line << "]" << std::endl;
+    }
+  }
+
+  infile.close();
+
+  // update bounding box
+  update(_dbh, (char *) "ALTER TABLE db ADD bbox mbr");
+  update(_dbh, (char *) "UPDATE db SET bbox = mbr(origin_airport)");
+
+#endif // __GNUC__
 }
 
 void MonetDBCtn::create_small_twitter() {
