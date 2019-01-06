b39c91e583696c108dfb1f78d2d193b68fa4d004
Cícero Pahins <cicerolp@gmail.com>
2019-01-06 09:13:17 -0400

    remove build files

--- benchmarks/build-release/cotire/nds_CXX_prefix.cxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.8.0 generated file */
-/* /mnt/sdb/Git/nds/benchmarks/build-release/cotire/nds_CXX_prefix.cxx */
-#ifdef __cplusplus
-#include "/mnt/sdb/Git/nds/source/stdafx.h"
-#endif
--- benchmarks/build-release/cotire/nds_CXX_prefix.hxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.8.0 generated file */
-/* /mnt/sdb/Git/nds/benchmarks/build-release/cotire/nds_CXX_prefix.hxx */
-#ifdef __cplusplus
-#include "/mnt/sdb/Git/nds/benchmarks/build-release/cotire/nds_CXX_prefix.cxx"
-#endif
